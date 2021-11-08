dfc56d1083e5cc951b688e640ebfb72d76e9d935
pawellula <pavloblack@hotmail.com>
2021-11-08 20:37:53 +0100

    Init

---
+++ README.md
@@ -0,0 +1,1 @@
+A small demo to showcase parcel bug with handling custom `.babelrc` config in a monorepo setup
---
+++ apps/web/src/index.js
@@ -0,0 +1,5 @@
+console.log("SHOULD BE REMOVED BY BABEL");
+
+export function add(a, b) {
+    return a + b;
+}
